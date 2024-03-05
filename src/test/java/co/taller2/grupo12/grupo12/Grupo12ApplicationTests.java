a08512e4d2bfe046fefa94ff31d3f3218f31f37c
amers_gos <juandiegopala_@outlook.es>
2024-03-05 11:49:26 -0500

    jpa
    primeros archivo para el taller de jpa

---
+++ src/test/java/co/taller2/grupo12/grupo12/Grupo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.taller2.grupo12.grupo12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
