57e76cd833fa308cfaf28d4200705114ec349d40
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2024-03-05 16:58:54 -0500

    Co-authored-by: Juan  Palacios Toledo <juanDiesmers@users.noreply.github.com>

---
+++ src/main/java/co/taller2/grupo12/grupo12/Controller/FormController.java
@@ -0,0 +1,50 @@
+package co.taller2.grupo12.grupo12.Controller;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import co.ApplicationRepository.ApplicationRepository;
+import co.wiki.project.grupo12.wiki.entity.Application;
+
+@Controller
+public class FormController {
+
+    private final ApplicationRepository applicationRepository;
+
+    @Autowired
+    public FormController(ApplicationRepository applicationRepository) {
+        this.applicationRepository = applicationRepository;
+    }
+
+    // @SuppressWarnings("null")
+    @PostMapping("/submit")
+    public String handleSubmitForm(Application formData) {
+        // Obtener los datos del formulario
+        String nombres = formData.getName();
+        String apellidos = formData.getApellidos();
+        String correo = formData.getCorreo();
+        String descripcion = formData.getDescripcion();
+        int semestre = formData.getSemestre();
+        int edad = formData.getEdad();
+
+        // Crear una nueva instancia de Application con los datos del formulario
+        LocalDate fecha = formData.getFecha();
+        Application application = new Application();
+        application.setName(nombres);
+        application.setApellidos(apellidos);
+        application.setCorreo(correo);
+        application.setDescripcion(descripcion);
+        application.setSemestre(semestre);
+        application.setEdad(edad);
+        application.setFecha(fecha);
+
+        // Guardar la nueva instancia en la base de datos
+        this.applicationRepository.save(application);
+
+        // Redirigir a otra página
+        return "Resultado";
+    }
+}
---
+++ src/main/java/co/taller2/grupo12/grupo12/Controller/HomeController.java
@@ -0,0 +1,43 @@
+package co.;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/grupo12/plantilla")
+public class HomeController {
+
+    @GetMapping("/Home")
+    public String losPipolSoloTexto() {
+        return "Home";
+    }
+
+    @GetMapping("/1presentacion")
+    public String losPipolSoloTexto1() {
+        return "1presentacion";
+    }
+
+    @GetMapping("/2Encuesta")
+    public ModelAndView losPipolTemplateModelAndView() {
+        return new ModelAndView("2Encuesta");
+    }
+
+    @GetMapping("/Resultado")
+    public String textoResultado() {
+        return "Resultado";
+    }
+
+    @GetMapping("/Requerimientos")
+    public String RequerimientosR() {
+        return "Requerimientos";
+    }
+
+    @GetMapping("/DescripcionProyecto")
+    public String DescripcionProyecto() {
+        return "DescripcionProyecto";
+    }
+}
