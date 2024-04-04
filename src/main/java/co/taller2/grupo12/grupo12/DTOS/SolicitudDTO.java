7530305538f36398e6da89aaae258def57663b2f
amers_gos <juandiegopala_@outlook.es>
2024-04-04 15:02:30 -0500

    solicitud completa

--- src/main/java/co/taller2/grupo12/grupo12/DTOS/SolicitudDTO.java
+++ src/main/java/co/taller2/grupo12/grupo12/DTOS/SolicitudDTO.java
@@ -1,16 +1,6 @@
 package co.taller2.grupo12.grupo12.DTOS;
 
 import java.sql.Date;
-import java.util.ArrayList;
-import java.util.List;
-
-import co.taller2.grupo12.grupo12.entity.Arrendatario;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,23 +10,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class SolicitudDTO {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id_solicitud;
-
-    @ManyToOne
-    @JoinColumn(name = "id_arrendatario", referencedColumnName = "id_arrendatario", unique=false, nullable=false)
-    private Arrendatario arrendatario;
-
-    @ManyToOne
-    @JoinColumn(name = "id_finca", referencedColumnName = "id_finca", unique=false, nullable=false)
-    private FincaDTO finca;
-
-    @OneToMany(mappedBy = "solicitud")
-    private List<PagoDTO> pagos = new ArrayList<PagoDTO>();
-
-    private Date fecha;
+    private Long idSolicitud;
     private String estado;
+    private Date fecha;
+    private Long idArrendatario;
+    private Long idFinca;
+
 }
