88f90f09d15d633f846af8e685ca1b625e9cac9b
Jairo Andrés <133978172+Jairo-Andres@users.noreply.github.com>
2024-05-26 19:42:17 -0500

    VerificacionCampos

--- src/main/java/co/taller2/grupo12/grupo12/DTOS/ArrendadorDTO.java
+++ src/main/java/co/taller2/grupo12/grupo12/DTOS/ArrendadorDTO.java
@@ -7,6 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
@@ -14,11 +15,22 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ArrendadorDTO {
     private Long id_arrendador;
+
+    @NotEmpty(message = "Nombre is mandatory")
     private String nombre;
+
+    @NotEmpty(message = "Apellido is mandatory")
     private String apellido;
+
+    @NotEmpty(message = "Correo is mandatory")
     private String correo;
+
+    @NotEmpty(message = "Telefono is mandatory")
     private String telefono;
+
+    @NotEmpty(message = "Contrasena is mandatory")
     private String contrasena;
-    private List<String> nombresFincas; 
+
+    private List<String> nombresFincas;
     private List<ComentarioDTO> comentarios = new ArrayList<>();
 }
--- src/main/java/co/taller2/grupo12/grupo12/DTOS/ArrendatarioDTO.java
+++ src/main/java/co/taller2/grupo12/grupo12/DTOS/ArrendatarioDTO.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
@@ -16,11 +17,22 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ArrendatarioDTO {
     private Long id_arrendatario;
+
+    @NotEmpty(message = "Nombre es obligatorio")
     private String nombre;
+
+    @NotEmpty(message = "Apellido es obligatorio")
     private String apellido;
+
+    @NotEmpty(message = "Correo es obligatorio")
     private String correo;
+
+    @NotEmpty(message = "Teléfono es obligatorio")
     private String telefono;
+
+    @NotEmpty(message = "Contraseña es obligatoria")
     private String contrasena;
+
     private List<Solicitud> solicitudes = new ArrayList<>();
     private List<Comentario> comentarios = new ArrayList<>();
 }
